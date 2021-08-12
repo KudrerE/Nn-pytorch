ba5d71433f0990967d759c6629a810cfdf00b027
Kudret Esmer <e2304608@gmail.com>
2021-08-12 10:35:13 +0300

    plot added

--- Neural_Network.py
+++ Neural_Network.py
@@ -12,6 +12,8 @@ from torchvision.io import read_image
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 
+
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
@@ -75,3 +77,4 @@ def test_loop(dataloader, model, loss_fn):
     test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    return test_loss,correct
--- main.py
+++ main.py
@@ -15,6 +15,9 @@ from Neural_Network import NeuralNetwork
 from Load_Dataset import CustomImageDataset
 from Neural_Network import train_loop
 from Neural_Network import test_loop
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
 
 learning_rate = 1e-3
 batch_size = 64
@@ -33,18 +36,31 @@ digit_dataset = CustomImageDataset(
     r"C:\Users\kudre\Desktop\mavhine_try_data\train.csv",
     transform=ToTensor())
 
-train, validate = torch.utils.data.random_split(digit_dataset, [38000, 4000],
-                                                generator=torch.Generator().manual_seed(42))
 
+
+
+
+train, validate = torch.utils.data.random_split(digit_dataset, [38000, 4000],
+                                                generator=torch.Generator().manual_seed(0))
 
 
 train_dataloader = DataLoader(train, batch_size=batch_size, shuffle=True)
 test_dataloader = DataLoader(validate, batch_size=batch_size, shuffle=True)
 
-epochs = 100
-
+epochs = 50
+losses= np.zeros((epochs))
+x=np.arange(epochs)
+acc=np.zeros((epochs))
+print(x)
 for t in range(epochs):
     print(f"Epoch {t + 1}\n-------------------------------")
     train_loop(train_dataloader, model, loss_fn, optimizer)
-    test_loop(test_dataloader, model, loss_fn)
+    losses[t],acc[t]=test_loop(test_dataloader, model, loss_fn)
+
 print("Done!")
+print(losses,x)
+plt.subplot(2,1,1)
+plt.plot(x,losses)
+plt.subplot(2,1,2)
+plt.plot(x,acc)
+plt.show()
